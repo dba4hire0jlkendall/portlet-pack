76f4d20567b72cb52482698c937a490a428258ec
kmenzli <kmenzli@exoplatform.com>
2014-05-11 19:10:45 +0100

    PRESALES-286 : [Mood Swing Portlet] Create Portlet Mood Swing

--- exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/MoodSwingApplication.java
+++ exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/MoodSwingApplication.java
@@ -4,12 +4,23 @@ import juzu.Path;
 import juzu.View;
 import juzu.request.RenderContext;
 import juzu.template.Template;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.ActivityManager;
+import org.exoplatform.social.core.manager.IdentityManager;
 
 import javax.inject.Inject;
 import java.io.IOException;
 
 public class MoodSwingApplication
 {
+
+    @Inject
+    ActivityManager activityManager;
+    @Inject
+    IdentityManager identityManager;
 
   /** . */
   @Inject
@@ -21,5 +32,41 @@ public class MoodSwingApplication
   {
       indexTemplate.render();
   }
+
+
+    /***
+     * Publish an activity *message* in the Activities Stream of user *username*
+     * @param username : username of the user in which the Activity will be deployed
+     * @param message : message to be published
+     * @throws Exception
+     */
+    private void addActivity (String username, String message) throws Exception {
+
+        //--- Create Social Identity
+        Identity identity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username,false);
+
+        //--- Create a Activity
+        ExoSocialActivity activity = new ExoSocialActivityImpl();
+
+        //---Set the activity Title (message de be displayed
+        activity.setTitle(message);
+
+        //--- Set the Identity in which th Activity will be displayed
+        activity.setUserId(identity.getId());
+
+        //TODO check if it is an update or a new Activity ?!
+
+        try {
+
+            //--- Invoke the ActivityManager to add a new Activity
+            activityManager.saveActivityNoReturn(identity, activity);
+
+        } catch (Exception e) {
+
+        }
+
+
+    }
+
 
 }
--- exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/package-info.java
+++ exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/package-info.java
@@ -7,6 +7,12 @@
                 @Stylesheet(src = "/org/exoplatform/addons/portlets/moodswing/assets/moodswing.css", location = AssetLocation.APPLICATION)
         }
 )
+@Bindings(
+        {
+                @Binding(value = org.exoplatform.social.core.manager.IdentityManager.class),
+                @Binding(value = org.exoplatform.social.core.manager.ActivityManager.class)
+        }
+)
 @Less(value = "moodswing.less", minify = true)
 
 @Application(defaultController = MoodSwingApplication.class)
@@ -17,5 +23,7 @@ import juzu.asset.AssetLocation;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
 import juzu.plugin.asset.Stylesheet;
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
 import juzu.plugin.less.Less;
 import juzu.plugin.portlet.Portlet;
