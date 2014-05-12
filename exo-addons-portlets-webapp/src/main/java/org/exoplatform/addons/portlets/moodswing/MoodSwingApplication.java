9a614605991ec24837409bff60ab6927c8c1759f
Esslem Ghodbane <eghodbane@exoplatform.com>
2014-05-12 04:42:49 +0100

    PRESALES-299 -  PRESALES-300

--- exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/MoodSwingApplication.java
+++ exo-addons-portlets-webapp/src/main/java/org/exoplatform/addons/portlets/moodswing/MoodSwingApplication.java
@@ -1,9 +1,20 @@
 package org.exoplatform.addons.portlets.moodswing;
 
 import juzu.Path;
+import juzu.Resource;
 import juzu.View;
 import juzu.request.RenderContext;
 import juzu.template.Template;
+import org.exoplatform.addons.statistics.api.services.StatisticsService;
+import org.exoplatform.addons.statistics.api.web.listener.StatisticsLifecycleListener;
+import org.exoplatform.commons.juzu.ajax.Ajax;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.ActivityManager;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.web.application.RequestContext;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -13,6 +24,20 @@ public class MoodSwingApplication {
     /**
      * .
      */
+    StatisticsService statisticsService;
+    public MoodSwingApplication() {
+        try {
+            statisticsService = StatisticsLifecycleListener.getInstance().getInstance(StatisticsService.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject
+    ActivityManager activityManager;
+    @Inject
+    IdentityManager identityManager;
+
     @Inject
     @Path("index.gtmpl")
     Template indexTemplate;
@@ -21,5 +46,27 @@ public class MoodSwingApplication {
     public void index(RenderContext renderContext) throws IOException {
         indexTemplate.render();
     }
+
+    @Ajax
+    @Resource
+    public void storeStatus(String status) throws Exception {
+       String remoteUser = RequestContext.getCurrentInstance().getRemoteUser();
+       statisticsService.addEntry("root", "", status, "Mood", "SWING", "", "", "", "");
+       System.out.println(">>>>>>>> DONE: " + statisticsService.getStatistics(0).size());
+       indexTemplate.render();
+    }
+
+    /*private void addActivity (String username, String message) throws Exception {
+
+        Identity identity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username,false);
+        ExoSocialActivity activity = new ExoSocialActivityImpl();
+        activity.setTitle(message);
+        activity.setUserId(identity.getId());
+        try {
+            activityManager.saveActivityNoReturn(identity, activity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 
 }
--- exo-addons-portlets-webapp/src/main/webapp/js/moodswing.js
+++ exo-addons-portlets-webapp/src/main/webapp/js/moodswing.js
@@ -1,21 +1,47 @@
-$(function() {
-    $( document ).ready(function(){
-        $("#selectImage").imagepicker({
-            hide_select: true
-        });
+//require(["SHARED/jquery", "SHARED/juzu-ajax"], function ($) {
 
-        $('#selectImage').on('change', function() {
-            $('#mood-swing').text(this.value);
-        });
+        $(document).ready(function () {
 
-        var $container = $('.image_picker_selector');
-        $container.imagesLoaded(function () {
-            $container.masonry({
-                columnWidth: 30,
-                itemSelector: '.thumbnail'
+            // ** Image Picker
+            $("#selectImage").imagepicker({
+                hide_select: true
             });
-        });
-    });
-});
 
+            $('#selectImage').on('change', function () {
+                $('#mood-swing').text(this.value);
+            });
+
+            var $container = $('.image_picker_selector');
+            $container.imagesLoaded(function () {
+                $container.masonry({
+                    columnWidth: 30,
+                    itemSelector: '.thumbnail'
+                });
+            });
+
+            // ** Get Data
+            $(".orange").live('click', function(event){
+             $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.storeStatus()", {"status":"orange"});
+             });
+             $(".green").live('click', function(event){
+             $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.storeStatus()", {"status":"green"});
+             });
+             $(".blue").live('click', function(event){
+             $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.storeStatus()", {"status":"blue"});
+             });
+             $(".pink").live('click', function(event){
+             $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.storeStatus()", {"status":"pink"});
+             });
+             $(".purple").live('click', function(event){
+             $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.storeStatus()", {"status":"purple"});
+             });
+
+            // ** Publish to Timeline
+           /* $(".bubble").live('click', function(event){
+                $('.MoodSwingPortlet').jzLoad("MoodSwingApplication.addActivity()", {"username":"root","message":$(".bubble").HTML()});
+            });
+*/
+
+        });
+//})($);
 
